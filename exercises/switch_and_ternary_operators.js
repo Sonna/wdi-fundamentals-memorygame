d8563c59f8182eb596fe8117120e47ab6c8a862e
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-18 07:53:16 +1000

    Add a copy of "Switch & Ternary" Exercise
    Add a copy of the JSBin "Switch and Ternary Operator" Exercise from
    General Assembly Unit 8.

---
+++ exercises/switch_and_ternary_operators.js
@@ -0,0 +1,42 @@
+// ## Exercise
+// - Write out a switch statement for the conditions that are written in this
+//   pseudocode:
+var favoriteMovie;
+var moviePhrase;
+
+switch (favoriteMovie) {
+// If favoriteMovie is "jaws"
+case "jaws":
+// Assign moviePhrase the value "You're gonna need a bigger boat."
+moviePhrase = "You're gonna need a bigger boat.";
+break;
+// Else if favoriteMovie is "the shining"
+case "the shining":
+// Assign moviePhrase the value "All work and no play makes Jack a dull boy."
+moviePhrase = "All work and no play makes Jack a dull boy.";
+break;
+// Else if favoriteMovie is "star wars"
+case "star wars":
+// Assign moviePhrase the value "Do. Or do not. There is no try."
+moviePhrase = "Do. Or do not. There is no try.";
+break;
+// Else if favoriteMovie is "forrest gump"
+case "forrest gump":
+// Assign moviePhrase the value "Life was like a box of chocolates."
+moviePhrase = "Life was like a box of chocolates."
+break;
+// Else if favoriteMovie is ack to the future"
+case "Back to the future":
+// Assign moviePhrase the value "Where we're going, we don't need roads."
+moviePhrase = "Where we're going, we don't need roads.";
+break;
+// Else
+default:
+// Assign moviePhrase the value "Great movie choice!"
+moviePhrase = "Great movie choice!";
+}
+
+// - Hit the "Run" button. Then type `moviePhrase;` into the "Console" panel and
+//   hit "return" to check and make sure you are getting the expected result.
+//   Try assigning different values to the `favoriteMovie` variable to make sure
+//   everything is working correctly.
