01195f06dcc25d7509f4bc31324278e97e1d7648
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-17 18:22:05 +1000

    Add Data Types GA exercise
    Add a copy of JSBin exercise for JavaScript Data Types, as requested by
    General Assembly.

---
+++ exercises/data_types.js
@@ -0,0 +1,46 @@
+// ## Exercise
+//
+// Write the code to perform the actions listed below in the **JavaScript**
+// panel of the
+// [JS Bin editor](http://jsbin.com/yoqefif/edit?html,css,js,output).
+//
+// ### Instructions
+//
+// - Create a variable `petName`. Assign (give) it the value `"Rover"`.
+var petName = "Rover";
+
+// - Create a variable `age`. Assign it the value `8`.
+var age = 8;
+
+// - Create a variable `favoriteToy`. Assign it the value `"ball"`.
+var favoriteToy = "ball";
+
+// - Hit the "Run" button in the "Console" panel and then check the values of
+//   the three variables you created by typing each variable name into the
+//   "Console" panel and hitting "return."
+// -- Example: petName;
+console.log(petName);
+
+// ## Exercise (Continued)
+// - Update `petName`. The new value should be `Arthur`.
+petName = "Arthur";
+
+// - Update `age`. The new age should be `5`.
+age = 5;
+
+// - Update `favoriteToy`. The new favorite toy should be `"yarn"`.
+favoriteToy = "yarn"
+
+// - Hit the "run" button in the "Console" panel and then check the values of
+//   the three variables you created by typing each variable name into the
+//   "Console" panel and hitting enter/return.
+// -- Example: `petName`;
+console.log(petName);
+
+// - In the "Console" panel, use the typeof command to find the type of data
+//   stored in each variable.
+// -- Example: `typeof petName`
+console.log(typeof petName);
+
+
+// Stuck? Check out the solutions in the cheat sheet at the end of this lesson.
