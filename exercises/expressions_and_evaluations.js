72f8ddb442adf3e76d25314aa64a6bf735984ca2
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-17 18:59:26 +1000

    Add a copy of JSBin Exercise for Expressions
    Add a copy of the JSBin Exercise for Expressions and Evaluations unit
    from General Assembly.

---
+++ exercises/expressions_and_evaluations.js
@@ -0,0 +1,41 @@
+// ## Exercise
+// Write the code to perform the actions listed below in the **JavaScript**
+// panel in the
+// [JS Bin editor](http://jsbin.com/ravahul/edit?html,css,js,output).
+//
+// In the "JavaScript" panel, declare (create) a variable `myNumber`. Assign it
+// the value `30`.
+var myNumber = 30;
+
+// - After declaring the `myNumber` variable in the "JavaScript" panel, be sure
+//   to check to make sure you've done things correctly by hitting "Run" in the
+//   "Console" panel and then typing `myNumber;`` in the "Console" panel and
+//   hitting the return/enter key to check its value. You'll want to do this
+//   after each step.
+console.log(myNumber);
+
+// Note: Ensure the _type_ of this value is correct — Remember `30` and `'30'`
+// are not the same! Here we want to store a number, so make sure that there are
+// no quotes around the value.
+//
+// - Reassign (update) the `myNumber` variable to `20`.
+myNumber = 20;
+
+// - Use the `+=` operator to add `5` to the current value of `myNumber`.
+myNumber += 5;
+
+// - Now create a second variable `greeting` and assign (give) it the value
+//   `"Hello"`.
+var greeting = "Hello";
+
+// - Create a third variable `name` and assign it the value `"Margaret"`.
+var name = "Margaret";
+
+// - Create a fourth variable `sayHello`. We want the variable to hold the value
+//   `"Hello Margaret"`. Use the variables `greeting` and `name` along with the
+//   `+` to create this value (referred to as string concatenation).
+var sayHello = greeting + ' ' + name;
+
+// Stuck? Check out the solutions in the Study Guide at the end of this lesson.
+// Not stuck? [slow clap] My you're a quick study, aren't you? See you in the
+// next lesson.
