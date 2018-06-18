3dca68c063ae705e4d316719f11256f3247190e0
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-18 11:10:54 +1000

    Update Memory Game push a value onto an Array
    Update Memory Game push a value onto an Array as per instructions in
    Unit 8 Assignment from General Assembly.
    
    == Quotes:
    > ## Instructions
    >
    > - Next up, you'll create a variable to represent the first card that
    > the user flipped.
    > -- Create a variable `cardOne`.
    > -- Store the first item in the `cards` array in the variable
    > `cardOne`.
    >
    > **HINT:** Use the array name, square brackets, and the index number to
    > retrieve this card from the array.
    >
    > - Now we'll want to add this first card to the `cardsInPlay` array.
    > -- Use the `push()` method to add `cardOne` to the end of the
    > `cardsInPlay` array.
    > -- Here's an example of the `push()` method if you need a syntax
    > refresher:
    >
    > ```javascript
    >   ghosts.push('clyde');
    > ```

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,5 +3,8 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-console.log("User flipped " + card[0]);
-console.log("User flipped " + card[3]);
+console.log("User flipped " + cards[0]);
+console.log("User flipped " + cards[3]);
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
