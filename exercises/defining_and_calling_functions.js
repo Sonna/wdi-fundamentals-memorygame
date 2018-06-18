7a916e000f07c82441e5b8c6c8c10c7fabf249ab
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-18 11:34:01 +1000

    Add copy JSBin Exercise Unit 9
    Add a copy of JSBin Exercise output as per instructions in Unit 9
    pre-work from General Assembly.
    
    == Quotes:
    > ## Exercise
    >
    > Write the code to perform the actions listed below in the "JavaScript"
    > panel in this [JS Bin
    > editor](http://jsbin.com/gikuni/edit?js,console,output).
    >
    > - In the "JavaScript" panel in JS Bin, define a function `recitePoem`.
    > -- Inside the function, log `"Roses are red, violets are blue."` to
    > the console.
    >
    > **HINT:** add `console.log("Roses are red, violets are blue.");`
    > inside the function.
    > - Call the `recitePoem` function in the "JavaScript" panel.
    >
    > **HINT:** make sure you use parentheses after the function name.
    > - Hit the "Run" button in the "Console" panel and you should see
    > `"Roses are red, violets are blue."` displayed in the "Console" panel.
    >
    > ## Exercise (Continued)
    >
    > - In the "JavaScript" panel in JS Bin, define a function `playSong`.
    > -- Inside the function, log `"Cheer up sleepy Jean, Oh, what can it
    > mean."` to the console.
    > -- Call the `playSong` function.
    > -- Hit the "Run" button in the "Console" panel and you should see
    > `"Cheer up sleepy Jean, Oh, what can it mean."` displayed in the
    > "Console" panel.
    >
    > - In the "JavaScript" panel in JS Bin, define a function `twoByFour`.
    > -- Inside the function, log `2 * 4` to the console.
    > -- Call the `twoByFour` function.
    > -- Hit the "Run" button in the "Console" panel and you should see `8`
    > displayed in the "Console" panel.
    >
    > Stuck? Check out the solutions in the Study Guide at the end of this
    > lesson. Not stuck? Woot! See you in the next lesson.

---
+++ exercises/defining_and_calling_functions.js
@@ -0,0 +1,41 @@
+//
+// ## Exercise
+//
+// Write the code to perform the actions listed below in the "JavaScript" panel in this [JS Bin editor](http://jsbin.com/gikuni/edit?js,console,output).
+//
+// - In the "JavaScript" panel in JS Bin, define a function `recitePoem`.
+// -- Inside the function, log `"Roses are red, violets are blue."` to the console.
+//
+// **HINT:** add `console.log("Roses are red, violets are blue.");` inside the function.
+// - Call the `recitePoem` function in the "JavaScript" panel.
+//
+// **HINT:** make sure you use parentheses after the function name.
+// - Hit the "Run" button in the "Console" panel and you should see `"Roses are red, violets are blue."` displayed in the "Console" panel.
+//
+var recitePoem = function () {
+  console.log("Roses are red, violets are blue.");
+}
+recitePoem();
+
+// ## Exercise (Continued)
+//
+// - In the "JavaScript" panel in JS Bin, define a function `playSong`.
+// -- Inside the function, log `"Cheer up sleepy Jean, Oh, what can it mean."` to the console.
+// -- Call the `playSong` function.
+// -- Hit the "Run" button in the "Console" panel and you should see `"Cheer up sleepy Jean, Oh, what can it mean."` displayed in the "Console" panel.
+//
+function playSong() {
+  console.log("Cheer up sleepy Jean, Oh, what can it mean.");
+}
+playSong();
+
+// - In the "JavaScript" panel in JS Bin, define a function `twoByFour`.
+// -- Inside the function, log `2 * 4` to the console.
+// -- Call the `twoByFour` function.
+// -- Hit the "Run" button in the "Console" panel and you should see `8` displayed in the "Console" panel.
+//
+// Stuck? Check out the solutions in the Study Guide at the end of this lesson. Not stuck? Woot! See you in the next lesson.
+var twoByFour = function() {
+  console.log(2 * 4);
+}
+twoByFour();
