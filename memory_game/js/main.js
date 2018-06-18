ea676986b1172cec45435ee2b5239ec2f31a1380
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-06-18 11:20:40 +1000

    Update Memory Game to alert when cards match
    Update Memory Game to alert when cards match as per instructions in Unit
    8 Assignment from General Assembly.
    
    == Quotes:
    > ## Instructions
    >
    > - Now we'll want to add logic that checks for equality between the two
    > cards in the `cardsInPlay` array.
    > -- Inside of the `if` statement you created in the last step, create
    > an `if...else` statement that checks for equality between the two
    > cards in the `cardsInPlay` array.
    > -- For the condition, check to see if the first card in the
    > `cardsInPlay` array (`cardsInPlay[0]`) is equal to the second card in
    > the `cardsInPlay` array.
    >
    > **HINT:** You'll want to use the `===` operator here.
    >
    > If values are equal, execute an `alert` with the message "You found a
    > match!"
    >
    > ```javascript
    > alert("You found a match!");
    > ```
    >
    > - If values are not equal, execute an `alert` with the message "Sorry,
    > try again."
    > - After saving your `main.js` file, open the index.html file in
    > Sublime Text, right click, and open the project in Chrome. The alert
    > "Sorry, try again." should pop up.
    > - Test out storing different cards in the `cardOne` and `cardTwo`
    > variables to make sure that everything is up and running!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,4 +11,10 @@ var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {}
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
